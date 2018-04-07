e7cf5fffc7eb6878d010ec4b261f541f31a56c54
Rafael Alencar <rafjaa@gmail.com>
2018-04-07 15:47:47 -0300

    Formata cabeçalho HTTP e realiza consulta

--- cliente/navegador.py
+++ cliente/navegador.py
@@ -9,8 +9,12 @@ def formata_consulta(url):
     }
 
     # Remove http:// e https:// do início caso seja domínio
-    url = url.strip('http://')
-    url = url.strip('https://')
+    url = url.lstrip('http://')
+    url = url.lstrip('https://')
+
+    # Remove a barra do domínio em casos 
+    # como https://www.google.com/:8000
+    url = url.replace('/:', ':')
 
     # Separa o host e o recurso solicitado
     partes = url.split('/')
@@ -23,16 +27,56 @@ def formata_consulta(url):
         dominio = dominio.split(':')[0]
 
     # Converte o domínio para IP, caso seja domínio
-    dados_consulta['ip'] = socket.gethostbyname(dominio)
-
+    try:
+        dados_consulta['ip'] = socket.gethostbyname(dominio)
+    except:
+        return {}
 
     # Verifica se há recurso solicitado
     if len(partes) > 1:
         dados_consulta['recurso'] = '/' + '/'.join(url.split('/')[1:])
 
-
-
     return dados_consulta
 
 
-print(formata_consulta('https://www.google.com:8000/user/1'))
+
+if __name__ == '__main__':
+
+    # Obtém a URL por linha de comando
+    try:
+        URL = sys.argv[1]
+
+        dados_consulta = formata_consulta(URL)
+
+        if not dados_consulta:
+            print('Forneça um endereço válido.')
+            sys.exit()
+        else:
+            print(dados_consulta)
+    except:
+        print('Modo de uso: python3 navegador.py endereco_site')
+        sys.exit()
+
+
+    HOST = dados_consulta['ip']
+    PORT = dados_consulta['porta']
+    RECURSO = dados_consulta['recurso']
+
+    # Formata o cabeçalho
+    header = 'GET {0} HTTP/1.1\nHost: {1}\n\n'.format(RECURSO, HOST).encode()
+
+    # Instancia o socket TCP IPv4
+    s = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM
+    )
+
+    s.connect((HOST, PORT))
+
+    try:
+        s.send(header)
+
+        print(s.recv(1024))
+
+    finally:
+        s.close()
