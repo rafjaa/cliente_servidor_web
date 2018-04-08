2a1f4f38a550fa33f4331451961ae73af5d36ceb
Rafael Alencar <rafjaa@gmail.com>
2018-04-08 19:41:13 -0300

    Servidor recebe requisição e envia resposta

--- servidor/servidor.py
+++ servidor/servidor.py
@@ -2,7 +2,27 @@ import os
 import socket
 import sys
 
+# Caso False, escuta em localhost
+USAR_IP_EXTERNO = False
 
+def obtem_ip_externo():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip_externo = s.getsockname()[0]
+        s.close()
+
+        return ip_externo
+    except:
+        return 'localhost'
+
+
+ip_servidor = 'localhost'
+if USAR_IP_EXTERNO:
+    ip_servidor = obtem_ip_externo()
+
+
+print(obtem_ip_externo())
 
 if __name__ == '__main__':
 
@@ -21,3 +41,30 @@ if __name__ == '__main__':
 
 
     print(porta, diretorio)
+
+
+    # Instancia o socket TCP IPv4
+    s = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM
+    )
+
+    # Permite que a porta do servido seja utilizada sucessivas vezes, 
+    # sem necessitar de aguardar um tempo de espera
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    s.bind((ip_servidor, porta))
+    s.listen(1)
+
+    print('Aguardando conexão em', ip_servidor, porta)
+
+    con, info_cliente = s.accept()
+
+    print('conexão efetuada por', info_cliente)
+
+    requisicao = con.recv(1024)
+
+    print(requisicao)
+
+    con.send('HTTP/1.1 200 OK\n\n<h1>It works!</h1>'.encode())
+
